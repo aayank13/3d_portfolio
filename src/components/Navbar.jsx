6eb8401d972f31aa9f65bbde2654a5ff9ba816c4
Aayan <aayank1306@gmail.com>
2024-01-07 23:33:23 +0530

    logo and title change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <header className="header">
         <NavLink to="/" className="w-12 h-12 items-center justify-center flex">
-            <img src="./public/aayank_image.png" className="rounded-full rotate-on-hover inline-block shadow-md"></img>
+            <img alt="logo" src="./public/aayank_image.png" className="rounded-full rotate-on-hover inline-block shadow-md"></img>
         </NavLink>
         <nav className="flex text-lg font-medium">
         <NavLink to="/contact" className={( {isActive }) => isActive ? 'text-blue-500' : 'text-black-500' }>
