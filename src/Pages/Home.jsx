2aea0c15bdec1abed0549336ba5ca16e51f014e7
Aayan <aayank1306@gmail.com>
2024-01-08 02:14:55 +0530

    bugs fixed for image not showing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <section  className='w-full h-screen relative'>
     <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'> 
-    <p> Right now, I'm building <a class="underline decoration-gray-400 text-black font-medium rounded-md hover:bg-gray-200 transition-colors motion-reduce:transition-none duration-200" href="https://learnexpo.app">@LearnExpo</a>: The Open Learning place. My hope is that one day, we'll be able to learn, teacb, and collaborate on the <span class="italic">web</span>.</p>
+    <p> Right now, I'm building <a class="underline decoration-gray-400 text-black font-medium rounded-md hover:bg-gray-200 transition-colors motion-reduce:transition-none duration-200" href="https://learnexpo.app">@LearnExpo</a>: The Open Learning place. My hope is that one day, we'll be able to learn, teach, and collaborate on the <span class="italic">web</span>.</p>
     </div>
     
       <Canvas>
