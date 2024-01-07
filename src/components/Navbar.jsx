689c7dad85904c8d4d7144b38e31566e4731bcd7
Aayan <aayank1306@gmail.com>
2024-01-08 02:26:18 +0530

    bugs fixed and folders setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from "react-router-dom";
-
+import aayank from "../assets/aayank_image.png";
 
 const Navbar = () => {
   return (
     <header className="header">
         <NavLink to="/" className="w-12 h-12 items-center justify-center flex">
-            <img class="transition-all sticky motion-reduce:transition-none duration-1000 animate-pulse motion-reduce:animate-none w-12 hover:animate-spin ease-in-out rounded-full" src="./public/aayank_image.png" loading="lazy" decoding="async" alt="Headshot"></img>
+            <img class="transition-all sticky motion-reduce:transition-none duration-1000 animate-pulse motion-reduce:animate-none w-12 hover:animate-spin ease-in-out rounded-full" src={aayank} loading="lazy" decoding="async" alt="Headshot"></img>
             <h2 class="ml-2 text-xl font-medium">Aayan</h2>
         </NavLink>
         <nav className="flex text-lg font-medium hover:underline">
