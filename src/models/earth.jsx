3aab04ae6ac944a22eeb706563603f3f486743cf
Aayan <aayank1306@gmail.com>
2024-01-09 19:57:11 +0530

    changed the earth color to white, will change background later

--- src/models/earth.jsx
+++ src/models/earth.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
-import { a } from '@react-spring/three';
+import { a } from "@react-spring/three";
 import { useThree } from "@react-three/fiber";
+import * as THREE from "three"; // Import THREE module
 
-import EarthSpace from '../assets/3d/earthquakes_-_2000_to_2019.glb';
+import EarthSpace from "../assets/3d/earthquakes_-_2000_to_2019.glb";
 
 const Earth = ({ isRotating, setIsRotating, ...props }) => {
   const EarthRef = useRef();
@@ -105,44 +106,23 @@ const Earth = ({ isRotating, setIsRotating, ...props }) => {
     };
   }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
 
+  // Create a white material for the Earth
+  const whiteMaterial = new THREE.PointsMaterial({ color: 0xffffff });
+
   return (
-        <a.group ref={EarthRef} {...props}>
-          <group rotation={[-Math.PI / 2, 0, 0]}>
-            <points
-              geometry={nodes.Object_2.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-            <points
-              geometry={nodes.Object_3.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-            <points
-              geometry={nodes.Object_4.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-            <points
-              geometry={nodes.Object_5.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-            <points
-              geometry={nodes.Object_6.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-            <points
-              geometry={nodes.Object_7.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-            <points
-              geometry={nodes.Object_8.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-            <points
-              geometry={nodes.Object_9.geometry}
-              material={materials["Scene_-_Root"]}
-            />
-          </group>
-        </a.group>
-      );
-    }
+    <a.group ref={EarthRef} {...props}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <points geometry={nodes.Object_2.geometry} material={whiteMaterial.clone()} />
+        <points geometry={nodes.Object_3.geometry} material={whiteMaterial.clone()} />
+        <points geometry={nodes.Object_4.geometry} material={whiteMaterial.clone()} />
+        <points geometry={nodes.Object_5.geometry} material={whiteMaterial.clone()} />
+        <points geometry={nodes.Object_6.geometry} material={whiteMaterial.clone()} />
+        <points geometry={nodes.Object_7.geometry} material={whiteMaterial.clone()} />
+        <points geometry={nodes.Object_8.geometry} material={whiteMaterial.clone()} />
+        <points geometry={nodes.Object_9.geometry} material={whiteMaterial.clone()} />
+      </group>
+    </a.group>
+  );
+};
 
 export default Earth;
