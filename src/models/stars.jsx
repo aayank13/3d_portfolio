e9e16b79ac035e98e541f172f1caeb93cdde3403
Aayan <aayank1306@gmail.com>
2024-01-09 17:56:55 +0530

    fixed stars size and visibility

--- src/models/stars.jsx
+++ src/models/stars.jsx
@@ -6,7 +6,7 @@ import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = (props) => {
   const ref = useRef()
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 100 }))
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10
     ref.current.rotation.y -= delta / 15
@@ -15,7 +15,7 @@ const Stars = (props) => {
     <mesh>
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
-        <PointMaterial transparent color="#12071f" size={0.005} sizeAttenuation={true} depthWrite={false} />
+        <PointMaterial transparent color="#12071f" size={0.7} sizeAttenuation={true} depthWrite={false} />
       </Points>
     </group>
     </mesh>
